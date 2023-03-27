4c29a53eeae31a2de094401f2498439bc1730b5a
genexusbot <builder@genexus.com>
2023-03-27 17:07:57 -0300

    Package version: 1.0.0-beta+20230327200102

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Calendar",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Calendar-1.0.0-beta+20230324153906.xcframework.zip",
-			checksum: "0c0c0b3c143a1ca346f07da392328e841d0f67617558216a97ce1036b207c46a"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Calendar-1.0.0-beta+20230327200102.xcframework.zip",
+			checksum: "1966876d46a48ac9b24fc7f809ce9cdf2c440f954aa66e7c0b633d383ddb8c06"
 		)
 	]
 )
