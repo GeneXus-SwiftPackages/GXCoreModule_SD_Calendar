282121980607ae3cb91085d0300e9e99a417f971
genexusbot <builder@genexus.com>
2023-08-15 10:22:51 -0300

    Package version: 1.1.0

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreModule_SD_Calendar",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreModule_SD_Calendar",
+			targets: ["GXCoreModule_SD_CalendarWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+	],
+	targets: [
+		.target(name: "GXCoreModule_SD_CalendarWrapper",
+				dependencies: [
+					"GXCoreModule_SD_Calendar",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreModule_SD_Calendar",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Calendar-1.1.0.xcframework.zip",
+			checksum: "cfe9fb4cf7a24a96b0dc93152ce820b2a178bfadd431ec3d777c18bec21fdf69"
+		)
+	]
+)
