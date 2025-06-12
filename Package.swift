3a187aceccbd7655fe4689ea5add4ae1bb66c7bd
The iOS Team @ GeneXus <builder@genexus.com>
2025-06-12 07:10:42 -0300

    Package version: 3.2.0-beta.14

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_CalendarWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.13")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.14")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_CalendarWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Calendar",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Calendar-3.2.0-beta.13.xcframework.zip",
-			checksum: "3b70236083ba33da9e20742a5ec6e878b6b6fee77e72937bd352a314f1751268"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Calendar-3.2.0-beta.14.xcframework.zip",
+			checksum: "ed1f8c3d42e19cc02e3d58365d26be0d9ed2d5641c86b594fbd339919449e668"
 		)
 	]
 )
